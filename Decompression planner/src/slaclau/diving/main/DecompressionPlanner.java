bf94d60a1a3b47a2aa8e20af36fe465c39249f7a
slaclau <seb.laclau@gmail.com>
2022-04-07 17:36:41 +0100

    Test

--- Decompression planner/src/slaclau/diving/main/DecompressionPlanner.java
+++ Decompression planner/src/slaclau/diving/main/DecompressionPlanner.java
@@ -18,7 +18,7 @@ public final class DecompressionPlanner {
 			Model model = new BuhlmannModelWithGF(simpleDive, new ZHL16B() );
 			ModelledDive dive = new ModelledDive(simpleDive, model);
 			dive.descend(40, 20);
-			dive.stay(210);
+			dive.stay(10);
 			dive.getDecompressionSchedule();
 		} catch (GasException e) {
 			e.printStackTrace();
