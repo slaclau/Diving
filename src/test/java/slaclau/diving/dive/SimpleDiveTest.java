6a3407903b8ced0de062cf07ecb5d050332b32f0
slaclau <seb.laclau@gmail.com>
2022-04-28 15:48:34 +0100

    Unit tests added for slaclau.diving.dive

--- src/test/java/slaclau/diving/dive/SimpleDiveTest.java
+++ src/test/java/slaclau/diving/dive/SimpleDiveTest.java
@@ -56,4 +56,41 @@ class SimpleDiveTest {
 			e.printStackTrace();
 		}
 	}
+	@Test
+	void testAscendStayDescend() {
+		try {
+			var dive = new SimpleDive(new Gas());
+			dive.descend(10,10);
+			assertTrue(dive.getPoint(0.5).isEqual(new Gas()));
+			assertEquals(dive.getPoint(0.5).getDepth(), 5);
+			
+			dive.stay(20);
+			assertTrue(dive.getPoint(10d).isEqual(new Gas()));
+			assertEquals(dive.getPoint(10d).getDepth(), 10);
+			
+			dive.switchGas(new Nitrox(.5));
+			assertTrue(dive.getPoint(21d).isEqual(new Gas()));
+			assertEquals(dive.getPoint(21d).getDepth(), 10);
+			
+			Exception e;
+			e = assertThrows(DiveTimeOutOfBoundsException.class, () -> {
+				dive.getPoint(22d);
+			});
+			e = assertThrows(DiveTimeOutOfBoundsException.class, () -> {
+				dive.getPoint(-1d);
+			});
+			
+			dive.stay(1);
+			assertTrue(dive.getPoint(22d).isEqual(new Nitrox(.5)));
+			assertEquals(dive.getPoint(22d).getDepth(), 10);
+			
+			dive.ascend(0,5);
+			assertTrue(dive.getPoint(23d).isEqual(new Nitrox(.5)));
+			assertEquals(dive.getPoint(23d).getDepth(), 5);
+			
+		} catch (GasException | DiveTimeOutOfBoundsException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
