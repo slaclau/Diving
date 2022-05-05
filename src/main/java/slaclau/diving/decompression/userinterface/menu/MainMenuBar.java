5df6c15a874902403e665bfe88c6b98df075e670
slaclau <seb.laclau@gmail.com>
2022-05-05 19:12:31 +0100

    feat(mainmenubar): added about JMenuItem to helpMenu JMenu

--- src/main/java/slaclau/diving/decompression/userinterface/menu/MainMenuBar.java
+++ src/main/java/slaclau/diving/decompression/userinterface/menu/MainMenuBar.java
@@ -1,7 +1,20 @@
 package slaclau.diving.decompression.userinterface.menu;
 
-import java.awt.MenuBar;
+import java.awt.event.KeyEvent;
 
-public class MainMenuBar extends MenuBar {
+import javax.swing.*;
 
+@SuppressWarnings("serial")
+public class MainMenuBar extends JMenuBar {
+	public MainMenuBar() {
+		createHelpMenu();
+	}
+	
+	private void createHelpMenu() {
+		JMenu helpMenu = new JMenu("Help");
+		JMenuItem about = new JMenuItem("About", KeyEvent.VK_T);
+		helpMenu.add(about);
+		add(helpMenu);
+	}
+	
 }
