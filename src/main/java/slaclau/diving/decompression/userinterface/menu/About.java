24d3f4bc8a238fc1d235e52a5e673eb9fe0a261b
slaclau <seb.laclau@gmail.com>
2022-05-05 20:55:12 +0100

    feat(about): layout changes, read version from version.properties

--- src/main/java/slaclau/diving/decompression/userinterface/menu/About.java
+++ src/main/java/slaclau/diving/decompression/userinterface/menu/About.java
@@ -3,33 +3,55 @@ package slaclau.diving.decompression.userinterface.menu;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
+import javax.swing.BoxLayout;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class About extends JDialog {
-	private JLabel versionLabel = new JLabel("Version"), version = new JLabel();
+	private JLabel product = new JLabel("Decompression planner");
+	private JLabel maintainer  = new JLabel("Sebastien Laclau");
+	private JLabel version = new JLabel();
+	private JLabel copyright = new JLabel();
 	
 	public About(JFrame jframe) {
 		super(jframe, "About");
-		int w = 300;
-		int h = 200;
-		setSize(w, h);
+		
+		JPanel panel = new JPanel();		
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		add(panel);
+		
+		version.setText("Version: not found");
+		
+		try (InputStream input = About.class.getClassLoader().getResourceAsStream("version.properties") ) {
+			Properties properties = new Properties();
+			if ( input == null ) return;
+			
+			properties.load(input);
+			version.setText("Version: " + properties.getProperty("version"));
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		panel.add(product);
+		panel.add(maintainer);
+		panel.add(version);
+		
+		pack();
+		
+		int w = getWidth();
+		int h = getHeight();
 		
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		int x = ( dim.width - w ) / 2;
 		int y = ( dim.height - h ) / 2;
 		setLocation(x,y);
-		
-		setLayout(new GridLayout(1,2));
-		
-		versionLabel.setPreferredSize(new Dimension(100,20));
-		version.setText("Test");
-		version.setPreferredSize(new Dimension(100,20));
-		add(versionLabel);
-		add(version);
 		
 		setVisible(true);
 	}
